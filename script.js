1cf24162ea351ce7e8fded390f26659403e60da9
ignaluis <ignaciolopez040700@gmail.com>
2024-07-01 13:27:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     var selectedCountry = localStorage.getItem("selectedCountry");
-    if (selectedCountry) {
-        alert("Bienvenido de nuevo! Has seleccionado: " + selectedCountry);
-    }
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        }
+    });
 });
 
 function selectCountry(country) {
     localStorage.setItem("selectedCountry", country);
-    alert("Has seleccionado: " + country);
-
-    if (country === 'Bolivia') {
-        window.location.href = 'https://pency.app/paradizestore';
+    var countryUrls = {
+        'Bolivia': 'https://pency.app/paradizestore.bo',
+        'Mexico': 'https://pency.app/paradizestore.mx',
+        'Chile': 'https://pency.app/paradizestore.cl',
+        'Argentina': 'https://pency.app/paradizestore.ar',
+        'Peru': 'https://pency.app/paradizestore.pen',
+        'Republica Dominicana': 'https://pency.app/paradizestore.rdp'
+    };
+    if (countryUrls[country]) {
+        window.location.href = countryUrls[country];
+    } else {
+        alert("País no reconocido");
     }
-    // Puedes agregar más redirecciones según el país si es necesario
 }
